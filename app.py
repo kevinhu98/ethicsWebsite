f6a8df888df691cad0bf087634e23086d9ede59f
Kevin Hu <kh2547@nyu.edu>
2019-12-02 02:06:33 -0500

    added second page and now takes name input

--- app.py
+++ app.py
@@ -2,17 +2,22 @@ import logging
 
 from flask import Flask, render_template, request, session, redirect, url_for, send_file
 
-
-
 app = Flask(__name__)
+app.secret_key = "password"
 app.jinja_env.auto_reload = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.run(debug=True, host='0.0.0.0')
 
-
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 def startpage():
-    return render_template("startpage.html")
+    if request.method == "POST":
+        session['firstName'] = request.form['firstName']
+        session['lastName'] = request.form['lastName']
+        return redirect(url_for('page1'))
+    elif request.method == "GET":
+        return render_template("startpage.html")
 
+@app.route("/page1")
+def page1():
+    return render_template("page1.html", lastName=session['lastName'])
 
-    
